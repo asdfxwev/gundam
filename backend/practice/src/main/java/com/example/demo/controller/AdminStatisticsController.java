60b0dcf126be2c3c520797e4a83a272900e5229b
Vaundy0824 <167267377+Vaundy0824@users.noreply.github.com>
2024-10-15 10:57:07 +0900

    df

--- backend/practice/src/main/java/com/example/demo/controller/AdminStatisticsController.java
+++ backend/practice/src/main/java/com/example/demo/controller/AdminStatisticsController.java
@@ -1,5 +1,19 @@
 package com.example.demo.controller;
 
-public class AdminStatisticsController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/adminstatistics")
+public class AdminStatisticsController {
+	
+	@GetMapping("/statisticsList")
+	public void statisticsList(Model model) {
+		
+	}
 }
